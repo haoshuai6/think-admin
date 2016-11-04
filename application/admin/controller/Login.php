d25b4c99022f01b919b0654fa237f63813e823bd
haoshuai_it <haoshuai_it@163.com>
2016-11-04 14:53:13 +0800

    update login model

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -1,15 +1,31 @@
 <?php
 namespace app\admin\controller;
 
+use think\Request;
+use think\View;
+use think\Config;
+
 /**
  *  用户登录、登出、验证码
  */
-class Center
+class Login
 {
+    public function __construct()
+    {
+        $this->view = View::instance(Config::get('template'), Config::get('view_replace_str'));
+    }
+
     /**
      * 登入
      */
     public function login_in() {
+      /*  if(Request::instance()->param(false)){ //无form提交,登录页面
+
+
+        }else{
+
+        }*/
+        return $this->view->fetch();
 
     }
     /**
