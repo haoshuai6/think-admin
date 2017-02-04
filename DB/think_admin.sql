64863148ce5d5041e2c50a76bdcfa2d0453f3ddc
haoshuai <haoshuai_it@163.com>
2017-02-04 16:05:20 +0800

    修改数据库文件

--- DB/think_admin.sql
+++ DB/think_admin.sql
@@ -12,9 +12,9 @@ MySQL - 5.5.40 : Database - think-blog
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`think-blog` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`think_admin` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `think-blog`;
+USE `think_admin`;
 
 /*Table structure for table `hsblog_article_category` */
 
